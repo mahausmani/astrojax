5865cadcfd200b935aed29142cbad9446b55b4df
Safi Haider <safihaider536@gmail.com>
2023-12-27 18:43:50 +0500

    fixed path issues

--- src/bayesian/layers.py
+++ src/bayesian/layers.py
@@ -3,8 +3,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from misc import ModuleWrapper
-from metrics import calculate_kl as KL_DIV
+from .misc import ModuleWrapper
+from .metrics import calculate_kl as KL_DIV
 
 class BayesianLinear(ModuleWrapper):
     def __init__(self, in_features, out_features, bias=True, priors=None):
