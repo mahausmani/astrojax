6faad2138d60fd5c4993edb510d732b90ccf947e
Safi Haider <safihaider536@gmail.com>
2024-01-02 13:48:55 +0500

    added colors to contour plots

--- src/utils.py
+++ src/utils.py
@@ -1,20 +1,11 @@
-import os
 import yaml
 import corner
-import numpy as np
 import matplotlib.pyplot as plt
-import torch
-from torch.nn import functional as F
 
 def load_config(config_file):
     with open(config_file, 'r') as file:
         config = yaml.safe_load(file)
     return config
-
-def adjust_learning_rate(optimizer, lr):
-    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
-    for param_group in optimizer.param_groups:
-        param_group['lr'] = lr
 
 def plot(outputs, targets, labels, filename=None):
     """Plot corner plot of the outputs and targets"""
@@ -30,7 +21,9 @@ def plot(outputs, targets, labels, filename=None):
             labels=labels, 
             truths=targets,
             quantiles=[0.16, 0.5, 0.84],
+            plot_contours=True,
             show_titles=True,
+            color="orange",
             title_kwargs={"fontsize": 12}
             )
     
